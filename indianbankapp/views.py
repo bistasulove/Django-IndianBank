fe7a9e990f49be78f8bfa04ab69a1e6b3dd59484
Sulove <bistasulove@gmail.com>
2019-02-14 22:26:10 +0530

    Modified views.py to use Response object of REST framework

--- indianbankapp/views.py
+++ indianbankapp/views.py
@@ -1,47 +1,44 @@
-from django.http import HttpResponse, JsonResponse
-from django.views.decorators.csrf import csrf_exempt
-from rest_framework.renderers import JSONRenderer
-from rest_framework.parsers import JSONParser
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 from .models import Banks, Branches
 from .serializers import BanksSerializer, BranchesSerializer
 
 
-@csrf_exempt
+@api_view(['GET', 'POST'])
 def bank_list(request):
     """List all the Banks or create new Bank"""
 
     if request.method == 'GET':
         banks = Banks.objects.all()
         serializer = BanksSerializer(banks, many=True)
-        return JsonResponse(serializer.data, safe=False)
+        return Response(serializer.data)
 
     elif request.method == 'POST':
-        data = JSONParser().parse(request)
-        serializer = BanksSerializer(data=data)
+        serializer = BanksSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return JsonResponse(serializer.data, status=201)
-        return JsonResponse(serializer.data, status=400)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.data, status=status.HTTP_400_BAD_REQUEST)
 
 
-@csrf_exempt
+@api_view(['GET', 'PUT', 'DELETE'])
 def bank_detail(request, pk):
     """Retrieve, Update or Delete a Bank"""
     try:
         bank = Banks.objects.get(pk=pk)
     except Banks.DoesNotExist:
-        return HttpResponse(status=404)
+        return Response(status=status.HTTP_404_NOT_FOUND)
 
     if request.method == 'GET':
         serializer = BanksSerializer(bank)
-        return JsonResponse(serializer.data)
+        return Response(serializer.data)
     elif request.method == 'PUT':
-        data = JSONParser().parse(request)
-        serializer = BanksSerializer(bank, data=data)
+        serializer = BanksSerializer(bank, data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return JsonResponse(serializer.data)
-        return JsonResponse(serializer.errors, status=400)
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     elif request.method == 'DELETE':
         bank.delete()
-        return HttpResponse(status=204)
+        return Response(status=status.HTTP_204_NO_CONTENT)

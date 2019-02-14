7191a94cbd22a02ae19424ac5e377f7d9148977a
Sulove <bistasulove@gmail.com>
2019-02-14 22:44:05 +0530

    Implemented Classed Base Views(mixed-in generics views of REST framework)

--- indianbankapp/urls.py
+++ indianbankapp/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from . import views
+from rest_framework.urlpatterns import format_suffix_patterns
 
 
 urlpatterns = [
-    path('banks/', views.bank_list ),
-    path('banks/<int:pk>/', views.bank_detail),
+    path('banks/', views.BankList.as_view() ),
+    path('banks/<int:pk>/', views.BankDetail.as_view()),
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
--- indianbankapp/views.py
+++ indianbankapp/views.py
@@ -1,44 +1,18 @@
-from rest_framework import status
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
+from rest_framework import generics
 from .models import Banks, Branches
 from .serializers import BanksSerializer, BranchesSerializer
 
 
-@api_view(['GET', 'POST'])
-def bank_list(request):
+class BankList(generics.ListCreateAPIView):
     """List all the Banks or create new Bank"""
 
-    if request.method == 'GET':
-        banks = Banks.objects.all()
-        serializer = BanksSerializer(banks, many=True)
-        return Response(serializer.data)
-
-    elif request.method == 'POST':
-        serializer = BanksSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.data, status=status.HTTP_400_BAD_REQUEST)
+    queryset = Banks.objects.all()
+    serializer_class = BanksSerializer
 
 
-@api_view(['GET', 'PUT', 'DELETE'])
-def bank_detail(request, pk):
+
+class BankDetail(generics.RetrieveUpdateDestroyAPIView):
     """Retrieve, Update or Delete a Bank"""
-    try:
-        bank = Banks.objects.get(pk=pk)
-    except Banks.DoesNotExist:
-        return Response(status=status.HTTP_404_NOT_FOUND)
 
-    if request.method == 'GET':
-        serializer = BanksSerializer(bank)
-        return Response(serializer.data)
-    elif request.method == 'PUT':
-        serializer = BanksSerializer(bank, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    elif request.method == 'DELETE':
-        bank.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+    queryset = Banks.objects.all()
+    serializer_class = BanksSerializer

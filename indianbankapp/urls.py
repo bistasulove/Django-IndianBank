17201fff6469391f91361b2ffceb424e69328871
Sulove <bistasulove@gmail.com>
2019-02-14 23:23:22 +0530

    Added a root view to route to both Banks and Branches

--- indianbankapp/urls.py
+++ indianbankapp/urls.py
@@ -2,11 +2,12 @@ from django.urls import path
 from . import views
 from rest_framework.urlpatterns import format_suffix_patterns
 
-urlpatterns = [
-    path('banks/', views.BankList.as_view()),
-    path('branches/', views.BranchList.as_view()),
-    path('banks/<int:pk>/', views.BankDetail.as_view()),
-    path('branches/<int:pk>/', views.BranchDetail.as_view()),
-]
+urlpatterns = format_suffix_patterns([
+    path('',views.api_root),
+    path('banks/', views.BankList.as_view(), name='bank-list'),
+    path('branches/', views.BranchList.as_view(), name='branch-list'),
+    path('banks/<int:pk>/', views.BankDetail.as_view(),name='bank-detail'),
+    path('branches/<int:pk>/', views.BranchDetail.as_view(),name='branch-detail'),
+])
 
-urlpatterns = format_suffix_patterns(urlpatterns)
+
--- indianbankapp/views.py
+++ indianbankapp/views.py
@@ -1,7 +1,18 @@
 from rest_framework import generics
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.reverse import reverse
 from .models import Banks, Branches
 from .serializers import BanksSerializer, BranchesSerializer
+from rest_framework import permissions
 
+
+@api_view(['GET'])
+def api_root(request, format = None):
+    return Response({
+        'banks': reverse('bank-list',request=request, format=format),
+        'branches': reverse('branch-list', request=request, format=format)
+    })
 
 class BankList(generics.ListCreateAPIView):
     """List all the Banks or create new Bank"""
